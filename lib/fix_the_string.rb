fb07dfe486f9afa89f88aecc70c09a7aee8de926
Branden <brandenehman45@gmail.com>
2022-03-26 15:15:15 -0600

    Completed and researched one solution

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,3 +1,16 @@
-def fix_the_string
-    
+def fix_the_string(str)
+    stack = []
+
+    str.each_char do |char|
+        if !stack.empty? && char != stack.last && char.upcase == stack.last.upcase
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+
+    result = ''
+    result = stack.pop + result until stack.empty?
+
+    result
 end
